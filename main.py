936d254a107d4af885f1a53c49aa4ca66e639fa3
Netza <l.lopeznetza@gmail.com>
2021-03-13 23:55:39 -0600

    cálculo de pagos del equipofc

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from typing import List
 from model import Jugadores
 from data import niveles,equipos
+from helpers import goles_por_equipo,goles_individuales,calcular_bono,calcular_sueldo_completo,alcance_total
+
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
@@ -11,8 +15,26 @@ app = FastAPI()
 async def pagos(jugadores: List[Jugadores]):
     
     
-    for item in jugadores:
-        print(item)
+    json_data = jsonable_encoder(jugadores)
+    totalGolesPorEquipo=0
+    totalGolesMinimoPorEquipo=0
+    totalBono=0
+    for item in json_data:
+        nivel=item['nivel']
+        minimo=niveles[nivel]
+        bono=item['bono']
+        sueldoFijo=item['sueldo']
+        totalGolesPorEquipo +=item['goles']
+        totalGolesMinimoPorEquipo +=minimo
+        individual=goles_individuales(item['goles'],minimo)
+        golesPorEquipo=goles_por_equipo(totalGolesPorEquipo,totalGolesMinimoPorEquipo)
+        alcanceTotal=alcance_total(individual,golesPorEquipo)
+        totalBono=calcular_bono(bono,alcanceTotal)
+        sueldoTotal=calcular_sueldo_completo(sueldoFijo,totalBono)
+        item['sueldo_completo']=sueldoTotal
+        item.update({"minimo": minimo})
+        item.pop("nivel") 
 
-             
-    return {"Jugadores": jugadores}
+
+
+    return JSONResponse(content={"Jugadores":json_data})
