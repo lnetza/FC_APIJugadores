85bad0cf9bd18b3f5eed8573dc16b0d59e32b815
Netza <l.lopeznetza@gmail.com>
2021-03-14 16:38:31 -0600

    se invoca a la función para calcular la suma de goles en equipo y minimos por equipo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from typing import List
 from model import Jugadores
 from data import niveles,equipos
-from helpers import goles_por_equipo,goles_individuales,calcular_bono,calcular_sueldo_completo,alcance_total
+from helpers import goles_por_equipo,goles_individuales,calcular_bono,calcular_sueldo_completo,alcance_total,suma_goles_equipo,suma_goles_minimo_equipo
 
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
@@ -14,27 +14,25 @@ app = FastAPI()
 @app.post("/jugadores/")
 async def pagos(jugadores: List[Jugadores]):
     
-    
     json_data = jsonable_encoder(jugadores)
-    totalGolesPorEquipo=0
-    totalGolesMinimoPorEquipo=0
     
+    totalGolesEquipo=suma_goles_equipo(json_data)
+    totalGolesMinimoPorEquipo=suma_goles_minimo_equipo(json_data)
+
     for item in json_data:
         nivel=item['nivel']
         minimo=niveles[nivel]
         bono=item['bono']
         sueldoFijo=item['sueldo']
-        totalGolesPorEquipo +=item['goles']
-        totalGolesMinimoPorEquipo +=minimo
         individual=goles_individuales(item['goles'],minimo)
-        golesPorEquipo=goles_por_equipo(totalGolesPorEquipo,totalGolesMinimoPorEquipo)
+        golesPorEquipo=goles_por_equipo(totalGolesEquipo,totalGolesMinimoPorEquipo)
         alcanceTotal=alcance_total(individual,golesPorEquipo)
         totalBono=calcular_bono(bono,alcanceTotal)
         sueldoTotal=calcular_sueldo_completo(sueldoFijo,totalBono)
         item['sueldo_completo']=sueldoTotal
-        item.update({"minimo": minimo})
-        item.pop("nivel") 
-
-
+        item.update({"goles_minimos": minimo})
+        item.pop("nivel")
+        
+        
 
     return JSONResponse(content={"Jugadores":json_data})
