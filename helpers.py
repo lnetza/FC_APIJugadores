3b0a0574f32901e2b917b3fb8e09d10add56e8fa
Netza <l.lopeznetza@gmail.com>
2021-03-14 16:35:50 -0600

    se agregan funciones para calcular la suma de goles en equipo y suma de los minimos por equipo

--- helpers.py
+++ helpers.py
@@ -1,3 +1,4 @@
+from data import niveles
 
 def goles_por_equipo(golesPorEquipo,golesMinimoPorEquipo):
     return (golesPorEquipo/golesMinimoPorEquipo)*100
@@ -6,10 +7,25 @@ def goles_individuales(goles,minimo):
     return 0 if goles<0 else (goles/minimo)*100
 
 def alcance_total(porcentajeIndividual,porcentajePorEquipo):
-    return ((porcentajeIndividual+porcentajePorEquipo)/2)/100
+    return (porcentajeIndividual+porcentajePorEquipo)/2
 
 def calcular_bono(bono,alcanceTotal):
-    return bono * alcanceTotal
+    return (alcanceTotal/100)*bono
 
 def calcular_sueldo_completo(sueldoFijo,totalBono):
     return round(sueldoFijo+totalBono,2)
+
+def suma_goles_equipo(data):
+    sumaGolesPorEquipo=0
+    for item in data:
+        sumaGolesPorEquipo +=item['goles']
+    return sumaGolesPorEquipo
+
+def suma_goles_minimo_equipo(data):
+    sumaGolesMinimoEquipo=0
+    for item in data:
+        nivel=item['nivel']
+        minimo=niveles[nivel]
+        sumaGolesMinimoEquipo +=minimo
+    return sumaGolesMinimoEquipo
+
