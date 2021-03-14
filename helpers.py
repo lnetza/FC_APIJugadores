7c10b68e8a4488f4b05ea4b4541203f63e734073
Netza <l.lopeznetza@gmail.com>
2021-03-13 23:24:39 -0600

    se crea helpers con las funciones para calcular bonos, alcance total, porcentaje indivual; por equipo y sueldo total

---
+++ helpers.py
@@ -0,0 +1,15 @@
+
+def goles_por_equipo(golesPorEquipo,golesMinimoPorEquipo):
+    return (golesPorEquipo/golesMinimoPorEquipo)*100
+
+def goles_individuales(goles,minimo):
+    return (goles/minimo)*100
+
+def alcance_total(porcentajeIndividual,porcentajePorEquipo):
+    return ((porcentajeIndividual+porcentajePorEquipo)/2)/100
+
+def calcular_bono(bono,alcanceTotal):
+    return bono * alcanceTotal
+
+def calcular_sueldo_completo(sueldoFijo,totalBono):
+    return round(sueldoFijo+totalBono,2)
