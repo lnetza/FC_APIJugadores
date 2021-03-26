ad653b817b2bac127a203c1c6e1cf311c0b12c40
Netza <l.lopeznetza@gmail.com>
2021-03-25 18:12:26 -0600

    Redireccion a docs

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ from helpers import *
 import json
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+from starlette.responses import RedirectResponse
 
 app = FastAPI(title="Resuelve FC")
 
+@app.get("/", include_in_schema=False)
+async def index():
+    return RedirectResponse(url="/docs")
 
 @app.post("/jugadores/")
 async def pagos(jugadores: Jugadores):
